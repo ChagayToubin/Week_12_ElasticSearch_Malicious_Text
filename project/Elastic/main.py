08d7860ffaf27aef095abad851189d911c887ba0
chagay <chagaytoubin@gmail.com>
2025-09-01 12:25:56 +0300

    finsih level1

---
+++ project/Elastic/main.py
@@ -0,0 +1,13 @@
+from project.Elastic.manager import Manager
+from project.data.data_processor import Data_process
+import os
+
+uri_es=os.getenv("URI","http://localhost:9200")
+
+data_to_send=Data_process.load_data()
+
+
+
+m=Manager(uri_es,data_to_send)
+m.manager_start()
+
